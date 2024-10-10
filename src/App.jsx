7580c68d77d477fb4631544366b26e5274188583
1ksn1ks <bonicpro27@gmail.com>
2024-10-10 19:56:48 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useRef, useEffect, useState } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './App.css'
+
+const INITIAL_CENTER = [
+  -63,
+  -65
+]
+const INITIAL_ZOOM = 7
+
+function App() {
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiMWtzbjFrcyIsImEiOiJjbTF3MDNmOHowaWU2MmpzN21mbnh0ZDk5In0.UIvnkHf4WouanqXhm5e8cQ'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: center,
+      zoom: zoom
+    });
+
+    mapRef.current.on('move', () => {
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter()
+      const mapZoom = mapRef.current.getZoom()
+
+      // update state
+      setCenter([ mapCenter.lng, mapCenter.lat ])
+      setZoom(mapZoom)
+    })
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
+  const handleButtonClick = () => {
+    mapRef.current.flyTo({
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM
+    })
+  }
+
+
+  return (
+    <>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
+      <button className='reset-button' onClick={handleButtonClick}>
+        Reset
+      </button>
+      <div id='map-container' ref={mapContainerRef} />
+    </>
+  )
+
+}
+
+export default App
